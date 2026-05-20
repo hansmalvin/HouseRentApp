431525538e80eb06ceced454db9f70f8fde5a37e
hansmalvin <161692298+hansmalvin@users.noreply.github.com>
2026-05-20 17:21:55 +0700

    property update problem fixed

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 8001;
 app.use(express.json());
 app.use(cors({
   origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   credentials: true
 }));
 app.use(cookieParser());
